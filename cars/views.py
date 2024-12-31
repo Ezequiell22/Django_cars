0d789dae289a79e8ac365087c6b8efc23d6a06e2
ezequiel <contatodevmais@gmail.com>
2024-12-30 22:20:04 -0300

    add form

--- cars/views.py
+++ cars/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from cars.models import Car
+from cars.forms import Carform
 
 def cars_view(request):
+    search = request.GET.get('search')
     
-    cars = Car.objects.all()
+    if not search:
+      search = ''
 
+    cars = Car.objects.filter(model__icontains=search).order_by('model')
+      
     return render(
         request,
         'cars.html',
         {"cars" : cars }   
     )
+
+def new_car_view(request):
+   new_car_form = Carform()
+   return render(request, 'new_car.html', {'new_car_form' :  new_car_form })
